3f65f59f7d53c70cac5c6ea2b8a9645d111b84a0
Byron <sixthbaron9@protonmail.com>
2026-02-09 07:58:23 -0500

    added more specific typing to `App.tsx`

--- features/App.tsx
+++ features/App.tsx
@@ -1,28 +1,46 @@
-import styles from '../features/App.module.css';
+import type {
+    ReactElement,
+    LiHTMLAttributes,
+    JSXElementConstructor,
+} from "react";
+import styles from "../features/App.module.css";
 
-const practiceCollections = (): Map<string, any> => {
+type CollectionType =
+    | number[]
+    | { name: string; age: number }
+    | Set<string>
+    | [boolean, string, number];
+
+const practiceCollections = (): Map<string, CollectionType> => {
     // Create a Map of collections to demonstrate collection usage
-    const collectionMap: Map<string, any> = new Map();
+    const collectionMap: Map<string, CollectionType> = new Map();
     collectionMap.set("numbersArray2", [1, 2, 3, 4, 5]);
-    // collectionMap.set("stringSet", new Set(["apple", "banana", "cherry"]));
+    collectionMap.set("stringSet", new Set(["apple", "banana", "cherry"]));
     collectionMap.set("userObject", { name: "Alice", age: 30 });
-    collectionMap.set("tupleExample", [true, "hello", 42] as [boolean, string, number]);
+    collectionMap.set("tupleExample", [true, "hello", 42] as [
+        boolean,
+        string,
+        number,
+    ]);
     return collectionMap;
-}
+};
 
-const displayCollections = (): any[] => {
-    let content: any [] = [];
-    const collections: Map<string, any> = practiceCollections();
-    collections.forEach((value: any, key: string) => {
-        content.push(<li key={key}>{ `${key}: ${JSON.stringify(value)}` }</li>);
+type listItem<T> = ReactElement<
+    LiHTMLAttributes<HTMLLIElement>,
+    T extends string ? string : JSXElementConstructor<any>
+>;
+
+const displayCollections = (): listItem<"li">[] => {
+    let content: listItem<"li">[] = [];
+    const collections: Map<string, CollectionType> = practiceCollections();
+    collections.forEach((value: CollectionType, key: string) => {
+        content.push(<li key={key}>{`${key}: ${JSON.stringify(value)}`}</li>);
     });
     return content;
 };
 
-const MyButton = ({title}: {title: string}) => {
-  return (
-    <button>{title}</button>
-  );
+const MyButton = ({ title }: { title: string }) => {
+    return <button>{title}</button>;
 };
 
 // const MyTextBox = ({content}: {content: string}) => {
@@ -32,13 +50,11 @@ const MyButton = ({title}: {title: string}) => {
 // };
 
 export default function MyApp() {
-  return (
-    <div className={styles.colContainer}>
-        <h1>Welcome to my app</h1>
-        <MyButton title='This is a button'/>
-        <ul>
-          {displayCollections()}
-        </ul>
-    </div>
-  );
+    return (
+        <div className={styles.colContainer}>
+            <h1>Welcome to my app</h1>
+            <MyButton title="This is a button" />
+            <ul>{displayCollections()}</ul>
+        </div>
+    );
 }
